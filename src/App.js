519061b195ca261a95a62fb0a0ba6c893bed4836
Miguel Lamas <lamasmigs@yahoo.co.uk>
2022-10-07 22:54:01 +0100

    Practicing - Outputting Lists in React

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import Navbar from "./Navbar"
-import Home from './Home';
+import React from 'react';
+
+import Navbar from "./components/Navbar.js";
+import Home from "./components/Home.js";
 
 function App() {
   return (
